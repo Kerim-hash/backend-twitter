3a4dfcc5292adef0e23871c2bf22028a58a163bc
Kerim <kerim.nurkaliev@sibers.com>
2022-03-24 20:30:19 +0600

    init

--- dist/validations/createTweet.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTweetValidations = void 0;
-var express_validator_1 = require("express-validator");
-exports.createTweetValidations = [
-    (0, express_validator_1.body)('text', 'Введите текст твита').isString().isLength({ max: 280 }).withMessage('Максимальная длина твита 280 символов'),
-];
